5f756c8f1b3bcc0279af91bba1ecfaa94e4ed357
wonbean08 <wonbean08@naver.com>
2026-03-20 10:10:34 +0900

    counter HTML onclick

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -2,13 +2,20 @@ let number = 0;
 
 // const resultH1 = document.getElementsByTagName("h1")[0];
 // const plusButton = document.getElementsByTagName("button")[0];
-const resultH1=document.getElementById("result");
-const plusButton=document.getElementsByClassName("plus")[0];
+const resultH1 = document.getElementById("result");
+
+// const plusButton=document.getElementsByClassName("plus")[0];
 // plusButton.addEventListener("click", () => {
 //     number++;
 //     resultH1.innerHTML = number;
 // });
-plusButton.onclick=() => {
+
+// plusButton.onclick=() => {
+//     number++;
+//     resultH1.innerHTML = number;
+// };
+
+const plus = () => {
     number++;
     resultH1.innerHTML = number;
-};
+}
