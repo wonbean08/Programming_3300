ded59d39833facebf4f0b318bea970c39c6c9fc9
wonbean08 <wonbean08@naver.com>
2026-03-25 10:09:07 +0900

    counter +, +2, -, reset

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,21 +1,12 @@
 let number = 0;
-
-// const resultH1 = document.getElementsByTagName("h1")[0];
-// const plusButton = document.getElementsByTagName("button")[0];
 const resultH1 = document.getElementById("result");
 
-// const plusButton=document.getElementsByClassName("plus")[0];
-// plusButton.addEventListener("click", () => {
-//     number++;
-//     resultH1.innerHTML = number;
-// });
+const plus = (value) => {
+    number+=value;
+    resultH1.innerHTML = number;
+}
 
-// plusButton.onclick=() => {
-//     number++;
-//     resultH1.innerHTML = number;
-// };
-
-const plus = () => {
-    number++;
+const reset = (value) => {
+    number=0;
     resultH1.innerHTML = number;
 }
