15334ca1c4b7c06f9bd4bee2530e174ea2c4d5f2
wonbean08 <wonbean08@naver.com>
2026-03-27 08:56:11 +0900

    count HTML 0 tkrwp, JS number 만 남기자, render() from ytm

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,12 +1,15 @@
-let number = 0;
+let number = 100;
 const resultH1 = document.getElementById("result");
+function render() { resultH1.innerHTML = number; }
 
-const plus = (value) => {
-    number+=value;
-    resultH1.innerHTML = number;
+render();
+
+const plus = (value = 1) => {
+    number += value;
+    render();
 }
 
 const reset = (value) => {
-    number=0;
-    resultH1.innerHTML = number;
+    number = 100;
+    render();
 }
