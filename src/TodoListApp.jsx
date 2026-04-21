750b8c5e7efeef8db48fba5f5c2ab581d7d51328
wonbean08 <wonbean08@naver.com>
2026-04-22 08:30:13 +0900

    TodoAdder

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,13 +1,25 @@
 import './todoList.css'
-import TodoItemEmpty from './components/TodoItemEmpty.jsx'
-import Button from './components/Button.jsx'
-import Checkbox from './components/Checkbox.jsx'
+
 import TodoHeader from './components/TodoHeader.jsx'
 import TodoAdder from './components/TodoAdder.jsx'
-import TodoItem from './components/TodoItem.jsx'
 import TodoList from './components/TodoList.jsx'
+import { useState } from 'react';
+
+class Todo{
+    constructor(text, isCompleted){
+        this.id=Date.now();
+        this.text=text;
+        this.isCompleted=isCompleted;
+    }
+}
 
 function TodoListApp() {
+
+    const [todos, setTodos]=useState([]);
+    const addTodo = (text) => setTodos((todos) => [
+        ...todos,new Todo(text)
+    ])
+
     return (
         <div className="todo">
             <TodoHeader/>
--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react"
 import Button from "./Button"
 
 
-export default function TodoAdder() {
+export default function TodoAdder({addTodo}) {
+    const [inputTodo, setInputTodo]=useState('');
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        if(!inputTodo) return;
+        addTodo(inputTodo.trim());
+        setInputTodo("");
+    }
     return (
         <>
-            <form className="todo_form">
-                <input type="text" placeholder="할일입력" className="todo_input" />
+            <form className="todo_form" onSubmit={handleSubmit}>
+                <input 
+                type="text" 
+                placeholder="할일입력" 
+                className="todo_input" 
+                onChange={(event) => setInputTodo(event.target.value)}
+                />
                 <Button className="todo_button todo_button--add" type="submit">add</Button>
             </form>
 
